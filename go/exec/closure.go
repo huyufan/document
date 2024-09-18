2088466ed8237db2b131251024cfe5284b569d76
小胡 <flat_boy@163.com>
2024-09-18 16:26:40 +0800

    闭包

--- go/exec/closure.go
+++ go/exec/closure.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
+	var x int = 1
+	y := func() {
+		x += 1
+
+	}
+
+	y()
+
+	fmt.Println(x, y)
 	var s []string = []string{"huyufan", "huyu", "fan"}
 	var wg sync.WaitGroup
 
@@ -18,4 +27,14 @@ func main() {
 	}
 	//注意由于匿名函数可以访问函数体外部的变量，而 for range 返回的 val 的值是引用的同一个内存地址的数据，所以匿名函数访问的函数体外部的 val 值是循环中最后输出的一个值
 	wg.Wait()
+
+	c, d := 1, 2
+
+	defer func(a int) {
+		fmt.Printf("c:%d,d:%d\n", a, d) // y 为闭包引用
+	}(c) // 复制 x 的值
+
+	c += 100
+	d += 100
+	fmt.Println(c, d)
 }
